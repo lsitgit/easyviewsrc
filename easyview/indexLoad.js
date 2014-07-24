fa656e597ee6e6b70d53286a9ac2b801136a318e
Alex Simes <alex.simes29@gmail.com>
2014-07-23 17:36:36 -0700

    made printer fixes

--- easyview/indexLoad.js
+++ easyview/indexLoad.js
@@ -12,7 +12,7 @@
         var COLUMNS = [  
 			{ header: 'Name (filter) all students',         locked: true, width:150,dataIndex:'last', resizable:true,
                                 renderer: function(v, cellValues, rec) {
-                                        return rec.get('last').replace(/\\'/g,"'") + ', ' + rec.get('first').replace(/\\'/g,"'");
+					return rec.get('last').replace(/\\'/g,"'") + ', ' + rec.get('first').replace(/\\'/g,"'");
                                 },
                                 editor: {
                                         xtype: 'textfield'
@@ -85,56 +85,64 @@
                 //column array points a gradeitem id to its readable name
                 var image ="";
                 if(grade_items[i]['type']!='category'){
-			image = "<img id='"+grade_items[i]['gid']+"' class='header-icon' style='vertical-align:middle;margin-bottom:4px;' src='resources/manual_item.svg'/>";
+                        image = "<img id='"+grade_items[i]['gid']+"' class='header-icon' style='vertical-align:middle;margin-bottom:4px;' src='resources/manual_item.svg'/>";
+
 		}
                 COLUMNS.push({header: image+grade_items[i]['name'], 
 				dataIndex: grade_items[i]['gid'], width:100,
 				hidden:hidden_var,
 				summaryType:function(records,values){//custom summary function that calculates averages without null values
-					var count = 0;
-					var total = 0;
-					var length = values.length;
-					for(var i =0; i< length;i++){
-						if(values[i]!=-99999){// the value -99999 indicates a null value for the grade, and the score shouldn't be considered
-							total = total+values[i];
-							count++;
-						}	
-					}
-					return total/count;
-				},
+                                        var count = 0;
+                                        var total = 0;
+                                        var length = values.length;
+                                        for(var i =0; i< length;i++){
+                                                if(values[i]!=-99999){// the value -99999 indicates a null value for the grade, and the score shouldn't be considered
+                                                        total = total+values[i];
+                                                        count++;
+                                                }
+                                        }
+                                        return total/count;
+                                },
                                 tooltip: grade_items[i]['name'], 
 				locked:grade_items[i]['locked'],//locked set in grade_grade_items function
 				itemId:(grade_items[i]['type']+i+grade_items[i]['cat_name']).replace(/["'()%\[\]{}\\=+\s&#@\^,\.!\$\*-]/g,''),
                                 renderer:function(value, metaData, record, rowIdx, colIdx, store, view){ 
-					var data = store.getAt(rowIdx);
-					var feedback_name = view.grid.columns[colIdx]['dataIndex'].substr(1)+'feedback';
-					var grade_name = ""+view.grid.columns[colIdx]['text'].replace(/<img[^>]*>/g,"")+"";
 					var new_value = value;
-					if(value == -99999){// the value -99999 indicates a null value for the grade
-						new_value =  "-";
+					if(value==-99999){// the value -99999 indicates a null value for the grade
+						new_value = "-";
 					}
-    					metaData.tdAttr = 'data-qtip="<strong>' + data.get('first').replace(/\\'/g,"'")+" "+data.get('last').replace(/\\'/g,"'")+
-						"<p>"+grade_name+", Score: "+new_value+
+					var data = store.getAt(rowIdx);
+					if(typeof view.grid === 'undefined'){//the grid printer calls this renderer but wont have access to view.grid, so we must run this code instead
+						var easygrid = Ext.ComponentQuery.query('#easyviewgrid')[0];
+						var feedback_name = easygrid.columns[colIdx]['dataIndex'].substr(1)+'feedback';
+						var grade_name = ""+easygrid.columns[colIdx]['text'].replace(/<img[^>]*>/g,"")+"";
+					}else{//otherwise, this code is needed (the stuff above wont work on initial rendering, possibly due to race conditions in initial rendering
+						var feedback_name = view.grid.columns[colIdx]['dataIndex'].substr(1)+'feedback';
+						var grade_name = ""+view.grid.columns[colIdx]['text'].replace(/<img[^>]*>/g,"")+"";
+					}
+    					metaData.tdAttr = 'data-qtip="<strong>' + data.get('first')+" "+data.get('last')+
+						"<p>"+grade_name+", Score: "+new_value+ 
 						"</strong><p>"+data.get(feedback_name) + '"';
-					return new_value;
+                                        return new_value;
                                 },
                                 summaryRenderer: function(value, summaryData, dataIndex) {
-					if(isNaN(value)){
-						return "-";
-					}
-					return Math.round(value*100)/100;
+                                        if(isNaN(value)){
+                                                return "-";
+                                        }
+                                        return Math.round(value*100)/100;
                                 }
 				
                 }); 
                 //model array defines which grade item ids each student should have a score for (specifying it as an int for now)
 		var typeformat="float";
-		if(grade_items[i]['gradetype']==2){
-			typeformat="string";
-		}
+                if(grade_items[i]['gradetype']==2){
+                        typeformat="string";
+                }
                 MODEL.push({name: grade_items[i]['gid'], type:typeformat});
                 MODEL.push({name: grade_items[i]['feedback'], type:'string'});
 		MODEL_FIELDS.push(grade_items[i]['gid']);
 		MODEL_FIELDS.push(grade_items[i]['feedback']);
+                
         }
         COLUMNS.push({header:"",width:1,itemId:'empty'});//add empty column to fix bug where avg gets overwritten by first student score
         FEWCOLUMNS = COLUMNS.slice(0,1).concat(COLUMNS.slice(4));
